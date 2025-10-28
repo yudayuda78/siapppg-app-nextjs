24e3b86ee568450ce61f362a303c9e673becf445
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-10-28 11:12:01 +0700

    Fix : event schema

--- prisma/migrations/20251028040931_edit_event/migration.sql
+++ prisma/migrations/20251028040931_edit_event/migration.sql
@@ -3,9 +3,22 @@ CREATE TABLE "event" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
+    "img" TEXT,
     "start" TIMESTAMP(3) NOT NULL,
     "end" TIMESTAMP(3) NOT NULL,
+    "linkDaftar" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "datasertifikat" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "instansi" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "kegiatan" TEXT NOT NULL,
+
+    CONSTRAINT "datasertifikat_pkey" PRIMARY KEY ("id")
 );
