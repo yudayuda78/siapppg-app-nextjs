5b9c45c9b9665ae91eee67558b4f2c8d701d3620
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-10-30 00:05:13 +0700

    Feat : verifikasi-sertifikat

---
+++ src/app/api/verifikasi-sertifikat/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const verifsertif = await prisma.datasertifikat.findMany()
+
+        return NextResponse.json(
+            {
+                success: true,
+                data: {
+                    verifsertif,
+                },
+                message: "Datasertifikat fetched successfully",
+            },
+            { status: 200 }
+        )
+        
+
+    }catch(error){
+        console.error("Error fetching sertifikat detail:", error);
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Failed to fetch verifikasi sertifikat",
+            },
+            { status: 500 }
+        )}
+}
